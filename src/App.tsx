e3d45ddd94e117354a499c08112060d86d6b85b0
Ricardo Francoli <francoliricardo@gmail.com>
2023-09-27 13:15:43 +0100

    feat(components): add page header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import Repos from '@/components/repos/Repos';
 
+import styles from './App.module.css';
+
 function App() {
-  return <Repos />;
+  return (
+    <div className={styles.app}>
+      <h1>Trending Repos</h1>
+      <h2>
+        See the top 30 trending 🚀 public repositories during the last week
+      </h2>
+      <p>
+        If you wanna see more (or less), insert a different number below (max
+        200)
+      </p>
+      <Repos />
+    </div>
+  );
 }
 
 export default App;
